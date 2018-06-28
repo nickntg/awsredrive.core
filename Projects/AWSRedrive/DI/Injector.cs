7ed0d6d6991fd8f050e856a00cdd3dd9ba6a576c
n.bitounis <n.bitounis@pamediakopes.gr>
2018-06-28 23:38:06 +0300

    Added MS DI and registered services.

---
+++ Projects/AWSRedrive/DI/Injector.cs
@@ -0,0 +1,28 @@
+﻿using AWSRedrive.Factories;
+using AWSRedrive.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AWSRedrive.DI
+{
+    public class Injector
+    {
+        private static ServiceProvider Container { get; }
+
+        static Injector()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfigurationReader, ConfigurationReader>();
+            services.AddSingleton<IQueueClientFactory, QueueClientFactory>();
+            services.AddSingleton<IMessageProcessorFactory, MessageProcessorFactory>();
+            services.AddTransient<IQueueProcessorFactory, QueueProcessorFactory>();
+            services.AddTransient<IConfigurationChangeManager, ConfigurationChangeManager>();
+            services.AddTransient<IOrchestrator, Orchestrator>();
+            Container = services.BuildServiceProvider();
+        }
+
+        public static IOrchestrator GetOrchestrator()
+        {
+            return Container.GetService<IOrchestrator>();
+        }
+    }
+}
