739a85445054872d0e60bd1b4edcbf723a1d4e6c
n.bitounis <n.bitounis@xe.gr>
2022-08-18 18:32:01 +0300

    GET request with JSON field unwrapping now supported.

--- Projects/AWSRedrive/HttpMessageProcessor.cs
+++ Projects/AWSRedrive/HttpMessageProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using AWSRedrive.Interfaces;
+using Newtonsoft.Json.Linq;
 using NLog;
 using RestSharp;
 using RestSharp.Authenticators;
@@ -15,9 +16,60 @@ namespace AWSRedrive
 
         public void ProcessMessage(string message, Dictionary<string, string> attributes, ConfigurationEntry configurationEntry)
         {
-            Logger.Trace($"Preparing post to {configurationEntry.RedriveUrl}");
+            Logger.Trace($"Preparing request to {configurationEntry.RedriveUrl}");
             var uri = new Uri(configurationEntry.RedriveUrl);
 
+            var options = CreateOptions(uri, configurationEntry);
+
+            var client = new RestClient(options);
+
+            var request = CreateRequest(message, uri, configurationEntry);
+
+            AddAuthentication(client, request, configurationEntry);
+
+            AddAttributes(request, attributes);
+
+            SendRequest(client, request, configurationEntry);
+        }
+
+        private RestRequest CreateRequest(string message, Uri uri, ConfigurationEntry configurationEntry)
+        {
+            return !configurationEntry.UseGET 
+                ? CreatePostOrPutRequest(message, uri, configurationEntry) 
+                : CreateGetRequest(message, uri);
+        }
+
+        private RestRequest CreateGetRequest(string message, Uri uri)
+        {
+            var request = new RestRequest(uri.PathAndQuery, Method.Get);
+            try
+            {
+                var data = JObject.Parse(message);
+                foreach (var p in data.Properties())
+                {
+                    request.AddQueryParameter(p.Name, p.Value.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Warn(ex, $"Error parsing message and adding query parameters. GET request might be incorrect.");
+                Logger.Warn($"Message was [{message}]");
+            }
+
+            return request;
+        }
+
+        private RestRequest CreatePostOrPutRequest(string message, Uri uri, ConfigurationEntry configurationEntry)
+        {
+            var request = new RestRequest(uri.PathAndQuery, configurationEntry.UsePUT ? Method.Put : Method.Post);
+
+            request.AddStringBody(message, DataFormat.Json);
+
+            return request;
+        }
+
+        private RestClientOptions CreateOptions(Uri uri, ConfigurationEntry configurationEntry)
+        {
             var options = new RestClientOptions($"{uri.Scheme}://{uri.Host}:{uri.Port}");
 
             if (configurationEntry.IgnoreCertificateErrors)
@@ -30,28 +82,19 @@ namespace AWSRedrive
                 options.Timeout = configurationEntry.Timeout.Value;
             }
 
-            var client = new RestClient(options);
+            return options;
+        }
 
-            var post = new RestRequest(uri.PathAndQuery, configurationEntry.UsePUT ? Method.Put : Method.Post);
-
-            post.AddStringBody(message, DataFormat.Json);
-
+        private void AddAuthentication(RestClient client, RestRequest request, ConfigurationEntry configurationEntry)
+        {
             if (!string.IsNullOrEmpty(configurationEntry.AwsGatewayToken))
             {
-                post.AddHeader("x-api-key", configurationEntry.AwsGatewayToken);
+                request.AddHeader("x-api-key", configurationEntry.AwsGatewayToken);
             }
 
             if (!string.IsNullOrEmpty(configurationEntry.AuthToken))
             {
-                post.AddHeader("Authorization", configurationEntry.AuthToken);
-            }
-
-            if (attributes != null)
-            {
-                foreach (var key in attributes.Keys.Where(key => !string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(attributes[key])))
-                {
-                    post.AddHeader(key, attributes[key]);
-                }
+                request.AddHeader("Authorization", configurationEntry.AuthToken);
             }
 
             if (!string.IsNullOrEmpty(configurationEntry.BasicAuthPassword) &&
@@ -60,12 +103,26 @@ namespace AWSRedrive
                 client.Authenticator = new HttpBasicAuthenticator(configurationEntry.BasicAuthUserName,
                     configurationEntry.BasicAuthPassword);
             }
+        }
 
+        private void AddAttributes(RestRequest request, Dictionary<string, string> attributes)
+        {
+            if (attributes != null)
+            {
+                foreach (var key in attributes.Keys.Where(key => !string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(attributes[key])))
+                {
+                    request.AddHeader(key, attributes[key]);
+                }
+            }
+        }
+
+        private void SendRequest(RestClient client, RestRequest request, ConfigurationEntry configurationEntry)
+        {
             Logger.Trace($"Posting to {configurationEntry.RedriveUrl}");
-            var response = client.ExecuteAsync(post).Result;
+            var response = client.ExecuteAsync(request).Result;
 
-            if (response.IsSuccessful && 
-                (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Created)) 
+            if (response.IsSuccessful &&
+                (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Created))
             {
                 Logger.Trace($"Post to {configurationEntry.RedriveUrl} successful");
                 return;
