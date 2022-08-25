f3adbe742c0978ce89ff2b6e05361d7141603694
n.bitounis <n.bitounis@xe.gr>
2022-08-25 09:49:03 +0300

    * Added some validations for HTTP verbs. * Fixed a validation error when Kafka topics are configured. * Added some tests for the validation logic.

--- Projects/AWSRedrive/Validations/ConfigurationEntryValidator.cs
+++ Projects/AWSRedrive/Validations/ConfigurationEntryValidator.cs
@@ -8,8 +8,11 @@ namespace AWSRedrive.Validations
         {
             RuleFor(x => x.QueueUrl).NotEmpty();
             RuleFor(x => x.RedriveUrl).NotEmpty().When(x => string.IsNullOrEmpty(x.RedriveScript) && string.IsNullOrEmpty(x.RedriveKafkaTopic));
-            RuleFor(x => x.RedriveScript).NotEmpty().When(x => string.IsNullOrEmpty(x.RedriveUrl) && string.IsNullOrEmpty(x.RedriveKafkaTopic));
-            RuleFor(x => x.RedriveKafkaTopic).NotEmpty().When(x => string.IsNullOrEmpty(x.RedriveUrl) && string.IsNullOrEmpty(x.RedriveScript));
+            RuleFor(x => x.RedriveScript).NotEmpty().When(x => string.IsNullOrEmpty(x.RedriveUrl) && !string.IsNullOrEmpty(x.RedriveKafkaTopic));
+            RuleFor(x => x.RedriveKafkaTopic).NotEmpty().When(x => string.IsNullOrEmpty(x.RedriveUrl) && !string.IsNullOrEmpty(x.RedriveScript));
+            RuleFor(x => x.UseGET).Equal(false).When(x => x.UseDelete || x.UsePUT);
+            RuleFor(x => x.UseDelete).Equal(false).When(x => x.UseGET || x.UsePUT);
+            RuleFor(x => x.UsePUT).Equal(false).When(x => x.UseGET || x.UseDelete);
         }
     }
 }
---
+++ Tests/AWSRedrive.Tests.Unit/ConfigurationEntryValidatorTests.cs
@@ -0,0 +1,105 @@
+﻿using AWSRedrive.Validations;
+using Xunit;
+
+namespace AWSRedrive.Tests.Unit
+{
+    public class ConfigurationEntryValidatorTests
+    {
+        [Fact]
+        public void NoQueueUrl()
+        {
+            var validator = new ConfigurationEntryValidator();
+            var entry = new ConfigurationEntry();
+            var result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Queue Url", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void NoRedriveUrl()
+        {
+            var validator = new ConfigurationEntryValidator();
+            var entry = new ConfigurationEntry
+            {
+                QueueUrl = "queue url"
+            };
+            var result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Redrive Url", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void NoRedriveScript()
+        {
+            var validator = new ConfigurationEntryValidator();
+            var entry = new ConfigurationEntry
+            {
+                QueueUrl = "queue url",
+                RedriveKafkaTopic = "kafka topic"
+            };
+            var result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Redrive Script", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void NoRedriveKafkaTopic()
+        {
+            var validator = new ConfigurationEntryValidator();
+            var entry = new ConfigurationEntry
+            {
+                QueueUrl = "queue url",
+                RedriveScript = "redrive script"
+            };
+            var result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Redrive Kafka", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void NoGetIfDeleteOrPut()
+        {
+            var validator = new ConfigurationEntryValidator();
+            var entry = new ConfigurationEntry
+            {
+                QueueUrl = "queue url",
+                RedriveUrl = "redrive url",
+                UseGET = true,
+                UsePUT = true
+            };
+            var result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Use GET", result.Errors[0].ErrorMessage);
+
+            entry.UsePUT = false;
+            entry.UseDelete = true;
+
+            result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Use GET", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void NoDeleteIfPut()
+        {
+            var validator = new ConfigurationEntryValidator();
+            var entry = new ConfigurationEntry
+            {
+                QueueUrl = "queue url",
+                RedriveUrl = "redrive url",
+                UseDelete = true,
+                UsePUT = true
+            };
+            var result = validator.Validate(entry);
+
+            Assert.False(result.IsValid);
+            Assert.Contains("Use Delete", result.Errors[0].ErrorMessage);
+        }
+    }
+}
